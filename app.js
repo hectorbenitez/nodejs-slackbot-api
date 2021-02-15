6a94354d69640bb8a6e1a7e25a8f1049b84730e7
Omar Morales <morales.ortega.omar@gmail.com>
2021-02-15 14:43:44 -0600

    mongoose connection added

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const surveyRouter = require("./src/routes/survey.router");
+
+// Mongoose connection
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
 
 app.set("port", process.env.PORT || 3001);
 app.set("json spaces", 2);
@@ -10,7 +19,8 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
+//routes
+app.use("/api/v1/", surveyRouter);
 
 //Starting the server
 app.listen(app.get("port"), () => {
