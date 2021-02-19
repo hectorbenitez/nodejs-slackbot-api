bce9ea842e2daebb55c0fb4ad5f0d6fce7e2ee24
Carlos Fabian <carl.fab.g@gmail.com>
2021-02-18 23:35:15 -0600

    add slack auth negotiation in slackSignin controller

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,14 +1,23 @@
+const axios = require('axios');
 
-
-const slackSignin = (req, res) => {
+const slackSignin = async (req, res) => {
     // slack token from client
     const { code } = req.body;
 
-   
+    // make a get call to get access token
+    const resp = await axios.get(`https://slack.com/api/oauth.v2.access?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&code=${code}`);
+    const { access_token } = resp.authed_user;
+
+    // use access token to retrieve user information
+    const userInfo = await axios.get('https://slack.com/api/users.identity', {
+        headers: {
+          'Authorization': `token ${access_token}`
+        }
+      });
 
     res.json({
         msg: 'Everything is okey',
-        token: id_token,
+        token: code,
     });
 };
 
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -4,27 +4,6 @@ const { slackSignin } = require('../controller/auth.controller');
 
 const router = Router();
 
-router.get('/auth/redirect', (req, res) =>{
-	var options = {
-  		uri: 'https://slack.com/api/oauth.access?code='
-  			+req.query.code+
-  			'&client_id='+process.env.CLIENT_ID+
-  			'&client_secret='+process.env.CLIENT_SECRET+
-  			'&redirect_uri='+process.env.REDIRECT_URI,
-		method: 'GET'
-  	}
-  	request(options, (error, response, body) => {
-  		var JSONresponse = JSON.parse(body)
-  		if (!JSONresponse.ok){
-  			console.log(JSONresponse)
-  			res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
-  		}else{
-  			console.log(JSONresponse)
-  			res.send("Success!")
-  		}
-  	})
-});
-
 router.post('/slack', [
     check('id_token', 'id_token is necesary').not().isEmpty()
 ], slackSignin);
